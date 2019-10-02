779e92f7a7c98f10c57e1366474bf029ef746ef2
Oluwa-femi <oluwatolablessing@gmail.com>
2019-10-02 10:06:34 +0100

    Userlist component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-
+import UserList from './component/UserList';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+    }
   }
   render() {
     return(
       <div className="App">
-
+        This is my app!
+        <UserList/>
       </div>
     )
   }
---
+++ my-app/src/component/UserList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+ class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>This is User List</div>;
+  }
+}
+
+export default UserList;
